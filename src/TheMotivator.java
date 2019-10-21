a75b3ddaa5dfdec530bd3ba321b4d82f7be6231a
michaelhumphrey <michaelhumphrey@mymail.mines.edu>
2019-10-21 17:20:22 -0600

    added another function call in AntiMotivator

--- src/TheMotivator.java
+++ src/TheMotivator.java
@@ -14,6 +14,7 @@ public class TheMotivator {
     public static void main(String[] args) {
         TheMotivator tm = new TheMotivator();
         tm.feedback(60);
+        tm.feedback(50);
     }
 }
 
