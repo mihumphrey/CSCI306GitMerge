69e0ff8b1abe03d814fcf461293597f1682f7752
michaelhumphrey <michaelhumphrey@mymail.mines.edu>
2019-10-21 17:18:55 -0600

    Added optimist branch/class

---
+++ src/Optimist.java
@@ -0,0 +1,14 @@
+public class Optimist {
+    public void feedback(int score) {
+        if (score == 100)
+            System.out.println("That's great");
+        else if (score > 90)
+            System.out.println("That's great");
+        else if (score > 60)
+            System.out.println("That's great ");
+        else if (score > 50)
+            System.out.println("That's great");
+        else
+            System.out.println("That's great");
+    }
+}
